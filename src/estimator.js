d09f64ff14558d2eef699411bb6203cc44eae4ec
Denver995 <denverclaude99@gmail.com>
2020-04-14 11:03:17 +0100

    feat: Starting with challenge3

--- src/estimator.js
+++ src/estimator.js
@@ -21,6 +21,30 @@ const infections = (periodType, currentlyInfected, timeToElapse) => {
   return result;
 };
 
+const dollarsInFlight = (dailyIncome, population, infected, periodType, period) => {
+  let time = 0;
+  let result = 0;
+  switch (periodType) {
+    case 'days':
+      time = Math.trunc(period);
+      break;
+    case 'weeks':
+      time = Math.trunc(period * 7);
+      break;
+    case 'months':
+      time = Math.trunc(period * 30);
+      break;
+    default:
+      time = 0;
+      break;
+  }
+
+  // const avgDailyIncome = dailyIncome / 100;
+  // const avgDailyIncomeP = population / 100;
+  result = Math.trunc((infected * dailyIncome * population) / time);
+  return result;
+};
+
 const covid19ImpactEstimator = (data) => {
   const input = data;
   const impact = {};
@@ -33,10 +57,10 @@ const covid19ImpactEstimator = (data) => {
   severeImpact.currentlyInfected = sci;
 
   // estimation of infectionsByRequestedTime
-  // const { periodType, timeToElapse } = data;
-  // const time = timeToElapse;
-  // const avgDailyIncomeP = input.region.avgDailyIncomePopulation;
-  // const avgUsd = input.region.avgDailyIncomeInUSD;
+  const { periodType, timeToElapse } = data;
+  const time = timeToElapse;
+  const avgDailyIncomeP = input.region.avgDailyIncomePopulation;
+  const avgUsd = input.region.avgDailyIncomeInUSD;
   const infectionI = infections(data.periodType, ici, data.timeToElapse);
   impact.infectionsByRequestedTime = infectionI;
   const infectionS = infections(data.periodType, sci, data.timeToElapse);
@@ -48,6 +72,25 @@ const covid19ImpactEstimator = (data) => {
   impact.severeCasesByRequestedTime = scbrtI;
   const scbrtS = Math.trunc(infectionS * 0.15);
   severeImpact.severeCasesByRequestedTime = scbrtS;
+
+  // estimation of hospitalBedsByRequestedTime
+  const bedI = Math.trunc((input.totalHospitalBeds * 0.35) - scbrtI);
+  impact.hospitalBedsByRequestedTime = bedI;
+  const bedS = Math.trunc((input.totalHospitalBeds * 0.35) - scbrtS);
+  severeImpact.hospitalBedsByRequestedTime = bedS;
+
+  // starting challenge 3
+  impact.casesForICUByRequestedTime = Math.trunc(infectionI * 0.05);
+  severeImpact.casesForICUByRequestedTime = Math.trunc(infectionS * 0.05);
+
+  const caseI = Math.trunc(infectionI * 0.02);
+  impact.casesForVentilatorsByRequestedTime = caseI;
+  const caseS = Math.trunc(infectionS * 0.02);
+  severeImpact.casesForVentilatorsByRequestedTime = caseS;
+
+  impact.dollarsInFlight = dollarsInFlight(avgUsd, avgDailyIncomeP, infectionI, periodType, time);
+  const dollarsInFlightS = dollarsInFlight(avgUsd, avgDailyIncomeP, infectionS, periodType, time);
+  severeImpact.dollarsInFlight = dollarsInFlightS;
 
   return {
     input,
